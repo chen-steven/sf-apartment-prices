4e0a30dc3a9a1602f5c5f7f300657ae1a923c1b5
Steven Chen <stevenchen@Stevens-MBP-3.stny.rr.com>
2021-11-11 00:29:13 -0500

    filters

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import { PricePlot } from "./PricePlot";
+import 'antd/dist/antd.css';
 
 function App() {
-  return <PricePlot/>
+  return (
+    
+    <div style={{
+      display: 'flex',
+      flexDirection: "column",
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+      <h1>SF Apartment Prices (two bed)</h1>
+      <PricePlot/>
+    </div>
+  );
+    
 }
 
 export default App;
--- src/PricePlot.js
+++ src/PricePlot.js
@@ -1,50 +1,76 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import Plot from 'react-plotly.js';
+import {Select} from "antd";
 
+const { Option } = Select;
 export function PricePlot() {
-    const [apartmentData, setApartmentData] = useState({})
+    const [apartmentData, setApartmentData] = useState()
+    const [key, setKey] = useState("min_price")
     useEffect(() => {
-        console.log('hello world')
         axios.get("https://36ej55h6pc.execute-api.us-east-1.amazonaws.com/Prod/get-apartment-data/")
         .then((res) => {
             const apartments = {
-                strata: {x: [], y: []},
-                windsor: {x: [], y: []}, 
-                "340-fremont": {x: [], y: []},
-                azure: {x: [], y: []}
+                strata: {date: [], min_price: [], max_price: [], mean_price: []},
+                windsor: {date: [], min_price: [], max_price: [], mean_price: []}, 
+                "340-fremont": {date: [], min_price: [], max_price: [], mean_price: []},
+                azure: {date: [], min_price: [], max_price: [], mean_price: []}
 
             }
             res.data.sort((a,b) => a.date.localeCompare(b.date))
             res.data.forEach((entry) => {
-                apartments[entry.name].x.push(entry.date)
-                apartments[entry.name].y.push(entry.min_price)
+                apartments[entry.name].date.push(entry.date)
+                apartments[entry.name].min_price.push(entry.min_price)
+                apartments[entry.name].max_price.push(entry.max_price)
+                apartments[entry.name].mean_price.push(entry.mean_price)
             })
             console.log(apartments)
             setApartmentData(apartments)
         })
     }, [])
+    
+    const onChange = (val) => {
+        setKey(val);
+    }
 
+    if (!apartmentData) {
+        return <h1>Loading...</h1>
+    }
 
-    const trace1 = {
-        ...apartmentData.windsor,
+    const windsor = {
+        x: apartmentData.windsor.date,
+        y: apartmentData.windsor[key],
         mode: "lines",
-        name: "windsor"
+        name: "Windsor"
     }
-    const trace2 = {
-        ...apartmentData.azure,
+    const azure = {
+        x: apartmentData.azure.date,
+        y: apartmentData.azure[key],
         mode: "lines",
-        name: "azure"
+        name: "Azure"
     }
-    const trace3 = {
-        ...apartmentData.strata,
+    const strata = {
+        x: apartmentData.strata.date,
+        y: apartmentData.strata[key],
         mode: "lines",
-        name: "strata"
+        name: "Strata"
     }
-    const trace4 = {
-        ...apartmentData["340-fremont"],
+    const threeFortyFremont = {
+        x: apartmentData["340-fremont"].date,
+        y: apartmentData["340-fremont"][key],
         mode: "lines",
-        name: "340-fremont"
+        name: "340 Fremont"
     }
-    return <Plot data={[trace1, trace2, trace3, trace4]}/>
+    
+    return (<>
+        <Select defaultValue="min_price" onChange={onChange}> 
+        <Option value="min_price">Min Price</Option>
+        <Option value="max_price">Max Price</Option>
+        <Option value="mean_price" >
+            Mean Price
+        </Option>
+        </Select>
+        <Plot data={[windsor, azure, strata, threeFortyFremont]}/>
+        
+        </>)
 }
